073c87ff693a67fd6a36ebde2870b9b4fbb1fe01
surekhakallutla <surekhakallutla@gmail.com>
2022-01-27 10:25:32 +0530

    Welcom to Address Book Management Program

---
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBook {
+
+}
---
+++ src/com/bridgelabz/addressbook/Main.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.addressbook;
+
+public class Main {
+	
+	static {
+		
+		System.out.println("Welcome to Address Book ManagementProgram");
+	}
+	
+	public static void main(String[] args) {
+		
+	
+	}
+
+}
